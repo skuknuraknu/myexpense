8bbf70612aaab4284925754baa74349a1ed3bfd4
Haeriskal Kamil <ahmaadm84112@gmail.com>
2025-09-28 12:13:12 +0700

    Feat: Add User entity

---
+++ src/main/kotlin/com/skuknuraknu/myexpense/entity/User.kt
@@ -0,0 +1,26 @@
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import java.time.LocalDateTime
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_users")
+class User {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    var id: Long? = 0
+
+    @Column(name="username", nullable=false, unique=true)
+    var username: String? = null
+
+    @Column(name="password", nullable=false)
+    var password: String? = null
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant? = Instant.now()
+
+    @Column(name = "updated_at")
+    var updatedAt: Instant? = Instant.now()
+    
+}
