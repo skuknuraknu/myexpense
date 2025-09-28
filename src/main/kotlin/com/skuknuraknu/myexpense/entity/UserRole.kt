7c43d14bbb787b3f9913285d01b24df1dfbcc85c
Haeriskal Kamil <ahmaadm84112@gmail.com>
2025-09-28 12:13:43 +0700

    Feat: Add UserRole Entity

---
+++ src/main/kotlin/com/skuknuraknu/myexpense/entity/UserRole.kt
@@ -0,0 +1,24 @@
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import java.time.LocalDateTime
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_user_roles")
+class UserRole {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    var id: Long? = 0
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User? = null
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    var role: Role? = null
+
+    @Column(name = "assigned_at", nullable = false)
+    var assignedAt: Instant? = Instant.now()
+}
